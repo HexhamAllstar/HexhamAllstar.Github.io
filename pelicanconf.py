43cb05749a995842732dee851eee2146c91fdcbd
HexhamAllstar <phil_bingham45@hotmail.com>
2018-03-07 23:39:52 +0000

    Changed theme to voidy-bootstrap

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,7 +9,7 @@ SITEURL = ''
 PATH = 'content'
 
 TIMEZONE = 'Europe/Paris'
-
+THEME = 'pelican-themes/voidy-bootstrap'
 DEFAULT_LANG = 'English'
 
 # Feed generation is usually not desired when developing
@@ -23,11 +23,11 @@ AUTHOR_FEED_RSS = None
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Download CV', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+          #('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
@@ -35,5 +35,25 @@ MARKUP = ('md', 'ipynb')
 
 PLUGIN_PATHS = ['./plugins']
 PLUGINS = ['ipynb.markup']
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+SITESUBTITLE ='A collection of data science/machine learning notebooks and anything else that I find interesting.'
+
+# Extra stylesheets, for bootstrap overrides or additional styling.
+STYLESHEET_FILES = ("pygment.css", "voidybootstrap.css",)
+
+# Put taglist at end of articles, and use the default sharing button implementation.
+CUSTOM_ARTICLE_FOOTERS = ("taglist.html", "sharing.html", )
+CUSTOM_SCRIPTS_ARTICLE = "sharing_scripts.html"
+CUSTOM_SIDEBAR_MIDDLES = ("sb_links.html", "sb_taglist.html", )
+
+# Default sidebar template. Omit this setting for single column mode without sidebar.
+SIDEBAR = "sidebar.html"
+
+SOCIAL = (('LinkedIn', 'http://www.linkedin.com/in/philbingham45/',
+         'fa fa-linkedin-square fa-fw fa-lg'),
+        ('GitHub', 'http://github.com/hexhamallstar',
+         'fa fa-github-square fa-fw fa-lg'),
+        )
